960b7cb0f8e29778c32517412f928cb72b2f5c7f
Randy Fay <randy@randyfay.com>
2023-10-28 09:21:39 -0600

    fix typo in settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -761,7 +761,7 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
   include __DIR__ . '/settings.ddev.php';
 }
 
-if file_exists(__DIR__ . '/settings.upsun.php')) {
+if (file_exists(__DIR__ . '/settings.upsun.php')) {
   include __DIR__ . '/settings.upsun.php';
 }
 

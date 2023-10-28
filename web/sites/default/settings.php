00cd9b7c940e9035755fd59b899f678ed123a3b8
Randy Fay <randy@randyfay.com>
2023-10-28 09:03:57 -0600

    move upsun settings into settings.upsun.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -761,6 +761,11 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
   include __DIR__ . '/settings.ddev.php';
 }
 
+if file_exists(__DIR__ . '/settings.upsun.php')) {
+  include __DIR__ . '/settings.upsun.php';
+}
+
+
 /**
  * Load local development override configuration, if available.
  *
@@ -779,19 +784,3 @@ if (getenv('IS_DDEV_PROJECT') == 'true' && file_exists(__DIR__ . '/settings.ddev
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 
-if (getenv('PLATFORM_PROJECT') != "") {
-    $databases['default']['default']['database'] = getenv('DB_PATH');
-    $databases['default']['default']['username'] = getenv('DB_USERNAME');
-    $databases['default']['default']['password'] = getenv('DB_PASSWORD');
-    $databases['default']['default']['host'] = getenv('DB_HOST');
-    $databases['default']['default']['driver'] = getenv('DB_SCHEME');
-    $databases['default']['default']['port'] = getenv('DB_PORT');
-    $settings['hash_salt'] = 'bf95508a645408b33848673dba1368d4f976bdb0ee4cd4d5b97dc9ddf9b88211';
-    if (empty($settings['config_sync_directory'])) {
-      $settings['config_sync_directory'] = 'sites/default/files/sync';
-    }
-    $settings['trusted_host_patterns'] = [
-      'platformsh\.site$',
-    ];
-
-}
